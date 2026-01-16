b70c0007c57ecf6b991cc6031f923ad682fd08f2
loo8ight <loo8ight@gmail.com>
2026-01-16 16:51:10 +0900

    Delete web directory

--- web/components/dashboard/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as StatCard } from './StatCard';
-export { default as RecentContentsTable } from './RecentContentsTable';
-export { default as UserContentStats } from './UserContentStats';
